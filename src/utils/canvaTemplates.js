c89428df1fc7fedff63d9748820032bc75631351
Erril <erril.aodesk@gmail.com>
2026-07-04 20:33:13 +0800

    fix: use working canva template search urls

--- src/utils/canvaTemplates.js
+++ src/utils/canvaTemplates.js
@@ -1,29 +1,29 @@
 export const CANVA_TEMPLATES = {
   facebook_post: {
-    'Announcement':             'https://www.canva.com/facebook-posts/templates/announcement/',
-    'Promotion / Sale':         'https://www.canva.com/facebook-posts/templates/sale/',
-    'New Product or Service':   'https://www.canva.com/facebook-posts/templates/product-launch/',
-    'Opening Hours Update':     'https://www.canva.com/facebook-posts/templates/business-hours/',
-    'Testimonial / Review':     'https://www.canva.com/facebook-posts/templates/testimonial/',
-    'Tip or Advice':            'https://www.canva.com/facebook-posts/templates/tips/',
-    'Event':                    'https://www.canva.com/facebook-posts/templates/event/',
-    'General Update':           'https://www.canva.com/facebook-posts/templates/',
+    'Announcement':           'https://www.canva.com/templates/?query=facebook+post+announcement',
+    'Promotion / Sale':       'https://www.canva.com/templates/?query=facebook+post+sale+promotion',
+    'New Product or Service': 'https://www.canva.com/templates/?query=facebook+post+product+launch',
+    'Opening Hours Update':   'https://www.canva.com/templates/?query=facebook+post+business',
+    'Testimonial / Review':   'https://www.canva.com/templates/?query=facebook+post+testimonial',
+    'Tip or Advice':          'https://www.canva.com/templates/?query=facebook+post+tips',
+    'Event':                  'https://www.canva.com/templates/?query=facebook+post+event',
+    'General Update':         'https://www.canva.com/templates/?query=facebook+post',
   },
   instagram_post: {
-    'Announcement':             'https://www.canva.com/instagram-posts/templates/announcement/',
-    'Promotion / Sale':         'https://www.canva.com/instagram-posts/templates/sale/',
-    'New Product or Service':   'https://www.canva.com/instagram-posts/templates/product-launch/',
-    'Opening Hours Update':     'https://www.canva.com/instagram-posts/templates/',
-    'Testimonial / Review':     'https://www.canva.com/instagram-posts/templates/testimonial/',
-    'Tip or Advice':            'https://www.canva.com/instagram-posts/templates/tips/',
-    'Event':                    'https://www.canva.com/instagram-posts/templates/event/',
-    'General Update':           'https://www.canva.com/instagram-posts/templates/',
+    'Announcement':           'https://www.canva.com/templates/?query=instagram+post+announcement',
+    'Promotion / Sale':       'https://www.canva.com/templates/?query=instagram+post+sale',
+    'New Product or Service': 'https://www.canva.com/templates/?query=instagram+post+product+launch',
+    'Opening Hours Update':   'https://www.canva.com/templates/?query=instagram+post+business',
+    'Testimonial / Review':   'https://www.canva.com/templates/?query=instagram+post+testimonial',
+    'Tip or Advice':          'https://www.canva.com/templates/?query=instagram+post+tips',
+    'Event':                  'https://www.canva.com/templates/?query=instagram+post+event',
+    'General Update':         'https://www.canva.com/templates/?query=instagram+post',
   },
   linkedin_post: {
-    'default': 'https://www.canva.com/linkedin-post/templates/',
+    'default': 'https://www.canva.com/templates/?query=linkedin+post',
   },
   twitter_post: {
-    'default': 'https://www.canva.com/twitter-post/templates/',
+    'default': 'https://www.canva.com/templates/?query=twitter+post',
   },
 }
 
@@ -33,6 +33,6 @@ export function getCanvaUrl(platform, postType) {
     platformTemplates[postType] ||
     platformTemplates['General Update'] ||
     platformTemplates['default'] ||
-    'https://www.canva.com/create/social-media/'
+    'https://www.canva.com/templates/?query=social+media+post'
   )
 }
